8d3ff614c6b5563a094945b0d1397852b016e08d
rodrigondec <rodrigondec@gmail.com>
2021-03-17 19:55:01 -0300

    melhora exemplos

--- examples/soap/confirma_retirada.py
+++ examples/soap/confirma_retirada.py
@@ -3,6 +3,6 @@ import os
 from as_ws_wrapper.wrapper.as_ws import AccesstageSoapWrapper
 from examples.utils import dump_response
 
-r = AccesstageSoapWrapper().confirma_retirada()
+r = AccesstageSoapWrapper().confirma_retirada("1", "t", "qwe")
 
 dump_response(r, os.path.basename(__file__).split(".")[0])
