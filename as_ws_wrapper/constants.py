d0950f0e4585bfd616999cc301c45f72f7265088
Rodrigo Castro <rodrigondec@gmail.com>
2021-03-17 19:51:51 -0300

    Update as_ws_wrapper/constants.py
    Co-authored-by: PedroRegisPOAR <pedroalencarregis@hotmail.com>

--- as_ws_wrapper/constants.py
+++ as_ws_wrapper/constants.py
@@ -1,7 +1,7 @@
 from decouple import config
 
 USERNAME = config("username", default="")
-"""Token básico para autenticação"""
+"""Nome do usuário para autenticação"""
 
 PASSWORD = config("password", default="")
 """Token básico para autenticação"""
