a7cb469f16552937557e1711740d61d1706d9c0a
rodrigondec <rodrigondec@gmail.com>
2021-03-17 14:06:32 -0300

    altera constants

--- as_ws_wrapper/constants.py
+++ as_ws_wrapper/constants.py
@@ -4,4 +4,4 @@ USERNAME = config("username", default="")
 """Token básico para autenticação"""
 
 PASSWORD = config("password", default="")
-"""developer_application_key"""
+"""Token básico para autenticação"""
