e8cd4a954f7631188bbde066ea1d1afce3505bfe
rodrigondec <rodrigondec@gmail.com>
2021-03-22 19:23:25 -0300

    altera models

--- as_ws_wrapper/models/base.py
+++ as_ws_wrapper/models/base.py
@@ -1,31 +1,32 @@
-from typing import Optional, Union
+from typing import Optional, Union, Literal
+from enum import IntEnum
 
 from pydantic import BaseModel, constr
 
 
 class IndividualInfo(BaseModel):
     document: constr(min_length=14, max_length=14)  # CPF/CNPJ do individuo
-    document_type: str  # tipo do documento 'CPF' ou 'CNPJ'
+    document_type: Literal["CPF", "CNPJ"]  # tipo do documento 'CPF' ou 'CNPJ'
     name: str  # nome do individuo
 
-    bank_account_bank_name: str  # nome do banco da conta bancária
-    bank_account_bank_code: str  # código do banco da conta bancária
-    bank_account_number: str  # número da conta bancária
-    bank_account_number_dv: str  # dv do númerp da conta bancária
-    bank_account_routing: str  # agência da conta bancária
-    bank_account_routing_dv: str  # dv da agência da conta bancária
+    bank_account_bank_name: Optional[str]  # nome do banco da conta bancária  | TED/DOC apenas
+    bank_account_bank_code: Optional[constr(min_length=3, max_length=3)]  # código do banco da conta bancária  | TED/DOC apenas
+    bank_account_number: Optional[str]  # número da conta bancária  | TED/DOC apenas
+    bank_account_number_dv: Optional[str]  # dv do númerp da conta bancária  | TED/DOC apenas
+    bank_account_routing: Optional[str]  # agência da conta bancária  | TED/DOC apenas
+    bank_account_routing_dv: Optional[str]  # dv da agência da conta bancária  | TED/DOC apenas
     bank_account_routing_number_dv: Optional[
         Union[str, None]
-    ]  # dv do agência/número da conta bancária
+    ]  # dv do agência/número da conta bancária  | TED/DOC apenas
 
 
 class TransactionModel(IndividualInfo, BaseModel):
     """ HEADER DO LOTE """
-
-    transaction_type: str  # identificador do tipo de transação
+    payment_type:  Literal["01", "03", "41", "43", "30", "31"]  # código do tipo de pagamento
 
     """ SEGMENTO A + B (DOC/TED) + J + J52 (boleto) """
-    number: str  # O que é o número do título para DOC/TED ?!?!?!?!
-    expiration_date: str  # data do vencimento
+    number: str  # Seu número, alfanumérico
     payment_amount: str  # quantia paga
-    code_line: str  # linha digitável do boleto
+    payment_date: str  # data de pagamento (formato DDMMAAAA)
+    code_line: Optional[str]  # linha digitável do boleto  | boleto apenas
+    expiration_date: Optional[str]  # data do vencimento (formato DDMMAAAA)  | boleto apenas
