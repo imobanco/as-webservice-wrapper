6c5987d9430f7816fc27334012277c8f521f3b13
rodrigondec <rodrigondec@gmail.com>
2021-03-29 13:43:14 -0300

    adiciona informações do pagador

--- as_ws_wrapper/models/base.py
+++ as_ws_wrapper/models/base.py
@@ -3,35 +3,54 @@ from typing import Optional, Union, Literal
 from pydantic import BaseModel, constr
 
 
-class IndividualInfo(BaseModel):
+class PayerInfo(BaseModel):
+    payer_document: constr(min_length=14, max_length=14)  # CPF/CNPJ do pagador
+    payer_document_type: Literal[
+        "CPF", "CNPJ"
+    ]  # tipo do documento 'CPF' ou 'CNPJ' do pagador
+    payer_name: str  # nome do pagador
+    payer_bank_account_number: str  # número da conta bancária do pagador  | TED/DOC apenas
+    payer_bank_account_number_dv: str  # dv do númerp da conta bancária do pagador  | TED/DOC apenas
+    payer_bank_account_routing: str  # agência da conta bancária do pagador  | TED/DOC apenas
+    payer_bank_account_routing_dv: str  # dv da agência da conta bancária do pagador  | TED/DOC apenas
+    payer_convenio: str  # convênio do pagador
+
+
+class ReceiverInfo(BaseModel):
     """
     Informações do favorecido/beneficiário
     """
 
-    document: constr(min_length=14, max_length=14)  # CPF/CNPJ do individuo
-    document_type: Literal["CPF", "CNPJ"]  # tipo do documento 'CPF' ou 'CNPJ'
-    name: str  # nome do individuo
+    receiver_document: constr(min_length=14, max_length=14)  # CPF/CNPJ do recebedor
+    receiver_document_type: Literal[
+        "CPF", "CNPJ"
+    ]  # tipo do documento 'CPF' ou 'CNPJ' recebedor
+    receiver_name: str  # nome do recebedor
 
-    bank_account_bank_name: Optional[
+    receiver_bank_account_bank_name: Optional[
         str
-    ]  # nome do banco da conta bancária  | TED/DOC apenas
-    bank_account_bank_code: Optional[
+    ]  # nome do banco da conta bancária do recebedor  | TED/DOC apenas
+    receiver_bank_account_bank_code: Optional[
         Union[constr(min_length=3, max_length=3), constr(min_length=0, max_length=0)]
-    ]  # código do banco da conta bancária  | TED/DOC apenas
-    bank_account_number: Optional[str]  # número da conta bancária  | TED/DOC apenas
-    bank_account_number_dv: Optional[
+    ]  # código do banco da conta bancária do recebedor  | TED/DOC apenas
+    receiver_bank_account_number: Optional[
         str
-    ]  # dv do númerp da conta bancária  | TED/DOC apenas
-    bank_account_routing: Optional[str]  # agência da conta bancária  | TED/DOC apenas
-    bank_account_routing_dv: Optional[
+    ]  # número da conta bancária do recebedor  | TED/DOC apenas
+    receiver_bank_account_number_dv: Optional[
         str
-    ]  # dv da agência da conta bancária  | TED/DOC apenas
-    bank_account_routing_number_dv: Optional[
+    ]  # dv do númerp da conta bancária do recebedor  | TED/DOC apenas
+    receiver_bank_account_routing: Optional[
+        str
+    ]  # agência da conta bancária do recebedor  | TED/DOC apenas
+    receiver_bank_account_routing_dv: Optional[
+        str
+    ]  # dv da agência da conta bancária do recebedor  | TED/DOC apenas
+    receiver_bank_account_routing_number_dv: Optional[
         Union[str, None]
-    ]  # dv do agência/número da conta bancária  | TED/DOC apenas
+    ]  # dv do agência/número da conta bancária do recebedor  | TED/DOC apenas
 
 
-class TransactionModel(IndividualInfo, BaseModel):
+class TransactionModel(ReceiverInfo, PayerInfo, BaseModel):
     """
     Informações da transação de pagamento
     """
@@ -43,8 +62,8 @@ class TransactionModel(IndividualInfo, BaseModel):
     ]  # código do tipo de pagamento
 
     """ SEGMENTO A + B (DOC/TED) + J + J52 (boleto) """
-    number: str  # Seu número, alfanumérico
-    payment_amount: str  # quantia paga
+    number: constr(max_length=20)  # Numeração única e alfanumérica
+    payment_amount: int  # quantia paga
     payment_date: str  # data de pagamento (formato DDMMAAAA)
     code_line: Optional[str]  # linha digitável do boleto  | boleto apenas
     expiration_date: Optional[
