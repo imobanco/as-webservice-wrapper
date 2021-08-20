1c9fd4518e72d10c6197d1bbfcd126bd38f1119b
rodrigondec <rodrigondec@gmail.com>
2021-08-20 13:09:19 -0300

    adiciona nix

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Flake do as-webservice-wrapper";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+
+        pkgsAllowUnfree = import nixpkgs {
+          system = "x86_64-linux";
+          config = { allowUnfree = true; };
+        };
+
+        config = {
+          projectDir = ./.;
+        };
+
+        hack = pkgsAllowUnfree.writeShellScriptBin "hack" ''
+          # Dont overwrite customised configuration
+
+          # https://dev.to/ifenna__/adding-colors-to-bash-scripts-48g4
+          echo -e '\n\n\n\e[32m\tAmbiente pronto!\e[0m\n'
+          echo -e '\n\t\e[33mignore as proximas linhas...\e[0m\n\n\n'
+        '';
+      in
+      {
+
+        poetryEnv = import ./mkPoetryEnv.nix.nix {
+          pkgs = nixpkgs.legacyPackages.${system};
+        };
+
+        env = pkgsAllowUnfree.poetry2nix.mkPoetryEnv config;
+
+        devShell = pkgsAllowUnfree.mkShell {
+          buildInputs = with pkgsAllowUnfree; [
+            #(pkgsAllowUnfree.poetry2nix.mkPoetryEnv config)
+            gnumake
+            hack
+            poetry
+            python3
+          ];
+
+          shellHook = ''
+            # TODO: documentar esse comportamento,
+            # devo abrir issue no github do nixpkgs
+            export TMPDIR=/tmp
+
+            echo "Entering the nix devShell"
+            hack
+          '';
+        };
+      });
+}
