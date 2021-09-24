9a7ecad7f3952dc77f2cb39e1e289a70de308778
Rodrigo Castro <rodrigondec@gmail.com>
2021-09-24 17:16:34 -0300

    Ajustes da Accesstage (#8)
    * adiciona campos
    
    * adiciona id de service na listagem
    
    * adiciona instruções
    
    * adiciona scripts de exemplo
    
    * Update examples/soap/mensagens_do_dia.py
    
    Co-authored-by: PedroRegisPOAR <pedroalencarregis@hotmail.com>
    
    * Update examples/soap/mensagens_do_dia_parseadas.py
    
    Co-authored-by: PedroRegisPOAR <pedroalencarregis@hotmail.com>
    
    * fmt
    
    * corrige teste
    
    * corrige testes
    
    Co-authored-by: PedroRegisPOAR <pedroalencarregis@hotmail.com>

--- examples/soap/recupera_mensagem.py
+++ examples/soap/recupera_mensagem.py
@@ -3,8 +3,8 @@ import os
 from as_ws_wrapper.wrapper.as_ws import AccesstageSoapWrapper
 from examples.utils import dump_response
 
-r = AccesstageSoapWrapper().recupera_mensagem("202108102896075")
+r = AccesstageSoapWrapper().recupera_mensagem("202108308901467")
 
-r.data["dscConteudoMensagem"] = r.data["dscConteudoMensagem"].decode()
+r.data["dscConteudoMensagem"] = r.data["dscConteudoMensagem"].decode("iso-8859-1")
 
 dump_response(r.data, os.path.basename(__file__).split(".")[0])
--- examples/utils.py
+++ examples/utils.py
@@ -11,6 +11,15 @@ def dump_response(response, file_name):
         json.dump(response, file, indent=4)
 
 
+def read_json(file_name):
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    data_path = os.path.join(current_path, "data")
+
+    file_path = os.path.join(data_path, f"{file_name}.json")
+    with open(file_path, "r") as file:
+        return json.load(file)
+
+
 def dump_csv(csv_string, file_name):
     current_path = os.path.dirname(os.path.realpath(__file__))
     data_path = os.path.join(current_path, "data")
